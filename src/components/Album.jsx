98672cd400d0c7f1f1eb93d9548af81aac6693c9
Cornelia Dahlin <154337428+lunek1@users.noreply.github.com>
2024-03-06 14:48:34 +0100

    Added comma between artists

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,22 +1,22 @@
-//import
-// import { ArtistName } from "./ArtistName";
-// import { CoverImage } from "./CoverImage";
-
-import { AlbumName } from "./AlbumName"
+import { AlbumName } from "./AlbumName";
 import { ArtistName } from "./ArtistName";
-import { CoverImage } from "./CoverImage"
+import { CoverImage } from "./CoverImage";
 
-export const Album =({albumData}) =>{
-    const artistDatas = albumData.artists;
-    return (
-        <div>
-            <CoverImage coverImage={albumData.images[1].url} /> 
-            <AlbumName albumName={albumData.name} albumLink={albumData.external_urls.spotify} />
-            {artistDatas.map ((name,external_url)=>(
-                <ArtistName key = {artistDatas.id} artistName={name} artistLink={external_url} />
-            ))}
-        </div>
+export const Album = ({ albumData }) => {
+  const artistNames = albumData.artists.map(artist => artist.name).join(', ');
 
-    )
-}
+  return (
+    <div>
+      <CoverImage coverImage={albumData.images[1].url} />
+      <AlbumName
+        albumName={albumData.name}
+        albumLink={albumData.external_urls.spotify}
+      />
+      <ArtistName name={artistNames} external_url={albumData.artists[0].external_urls.spotify} />
+    </div>
+  );
+};
+
 //key = {albumData.images} for images?
+
+//it map() the items array, artist and images need to be map in Album component. key?
